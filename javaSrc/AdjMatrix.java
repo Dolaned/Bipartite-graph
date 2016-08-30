d6ce4908d2571e4526771ac1aa3c7846f3b6b9db
therobotdoctor <michaelbannister01@hotmail.com>
2016-08-31 00:26:48 +1000

    Add files via upload
    AdjMatrix passes 3 tests but much slower than sample

--- javaSrc/AdjMatrix.java
+++ javaSrc/AdjMatrix.java
@@ -43,7 +43,7 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         //no duplicate keys, so check if vertce exists
     	if(vertices.get(vertLabel)!= null)
         {
-        	//assume program does nothing is repeated
+        	//assume program does nothing
     		//System.out.println("vertice already exsists");
         	return;
         }
@@ -57,15 +57,6 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         }
         else
         {
-        	/*
-        	 * int[][] temp = new int[volume][volume];
-        	
-        	System.arraycopy(edges, 0, temp, 0, edges.length);//temp = edges;
-        	volume = volume * 2;
-        	edges = new int[volume][volume];
-        	System.arraycopy(temp, 0, edges, 0, numOfV - 1);//edges = temp;
-        	*/
-        	
         	//gone for simple brute force solution to intialise array
         	//copy old array into tempary one
         	int[][] temp = new int[edges.length][edges.length];
@@ -89,257 +80,133 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         			edges[i][j] = temp[i][j]; 
         		}
         	}
-        	
-        	/*
-        	edges.clone();
-        	
-        	int [][] temp = new int[edges.length][];
-        	for(int i =0; i< edges.length;++i)
-        	{
-        		int[] aMatrix = edges[i];
-        		System.out.println(aMatrix.length);
-        		int   aLength = aMatrix.length;
-        		temp[i] = new int[aLength];
-        		System.arraycopy(aMatrix, 0, temp[i], 0, aLength);
-        	}
-        	
-        	volume = volume *2;
-        	System.out.println(volume);
-        	
-        	edges = new int[volume][];
-        	for(int i =0; i< volume;++i)
-        	{
-        		int[] aMatrix = temp[i];
-        		int   aLength = aMatrix.length;
-        		edges[i] = new int[volume];
-        		System.arraycopy(aMatrix, 0, edges[i], 0, aLength);
-        	}
-        	*/
-        	
-        	
-        	/*
-        	 * int [][] myInt = new int[matrix.length][];
-			for(int i = 0; i < matrix.length; i++)
-			{
-			  int[] aMatrix = matrix[i];
-			  int   aLength = aMatrix.length;
-			  myInt[i] = new int[aLength];
-			  System.arraycopy(aMatrix, 0, myInt[i], 0, aLength);
-			}
-			*/
-        	
         }
-        
-        /*
-        // need to cerate new array for edges, create new then copy from old
-        tempEdges = new int[numofV][numofV];
-        for(int i = 0;i<numOfV;i++)
-        {
-            for(int j=0;j<numOfV;j++)
-            {
-                tempEdges[i][j] = edges[i][j];
-            }
-        }
-        //replace array with newly sized array
-        edges = new int[numOfV][numOfV];
-
-        for(int i = 0;i<numOfV;i++)
-        {
-            for(int j=0;j<numOfV;j++)
-            {
-                edges[i][j] = tempEdges[i][j];
-            }
-        }*/
-
-
-        // Implement me!
     } // end of addVertex()
 	
     
-    public void addEdge(T srcLabel, T tarLabel) {
+    public void addEdge(T srcLabel, T tarLabel) throws IllegalArgumentException {
     	//check if both vertices exist within graph
-        boolean error = false;
-        if(vertices.get(srcLabel) == null)
-        {
-        	//System.out.println("error source vertice not found");
-        	error = true;
-        }
-        if(vertices.get(tarLabel) == null)
-        {
-        	//System.out.println("error target vertice not found");
-        	error = true;
-        }
-        if(error == true)
-        	return;
+    
+    	if(vertices.get(srcLabel) == null || vertices.get(tarLabel) == null)
+    	{
+    		throw new IllegalArgumentException("one or both of incident vertices"
+    				+ " does not exist");
+    	}
+    	
+    	//else get value for each vertice to add edge
+    	Integer x = vertices.get(srcLabel);
+    	Integer y = vertices.get(tarLabel);
+
+    	//then create edge
+    	//undirected graph, make sure to add edge in both directions
+    	edges[x][y] = 1;
+    	edges[y][x] = 1;
+    	// Implement me!
         
-        //else get value for each vertice to add edge
-        Integer x = (int) vertices.get(srcLabel);
-        Integer y = (int) vertices.get(tarLabel);
-        
-        
-        //redundant check
-        /*if(x==null||y==null){
-            System.out.println("vertice not found");
-        	return;
-            //error vertix not found
-        }*/
-        //perofrm more checks
-        //then create edge
-        //undirected graph, make sure to add edge in both directions
-        edges[x][y] = 1;
-        edges[y][x] = 1;
-        // Implement me!
     } // end of addEdge()
 	
 
-    public ArrayList<T> neighbours(T vertLabel) {
-        ArrayList<T> neighbours = new ArrayList<T>();
-        //String label = vertLabel.toString();
-        if(vertices.get(vertLabel) == null)
-        {
-        	//vertice does not exist within table; exit
-        	return null;
-        }
-        
-        //otherwise need to find out all the vertices attached
-        int v = (int) vertices.get(vertLabel);
-        //after finding column for V check all other V for edges
-        for(int i = 0; i <numOfV; ++i)
-        {
-        	if(edges[i][v] == 1)
-        	{
-        		//get key based on value then add to ArrayList
-        		neighbours.add(getKeyFromValue(i));
-        	}
-        }
-        // Implement me!
-        
-        return neighbours;
+    public ArrayList<T> neighbours(T vertLabel) throws IllegalArgumentException{
+    	ArrayList<T> neighbours = new ArrayList<T>();
+    	
+    	if(vertices.get(vertLabel) == null)
+    	{
+    		//vertice does not exist within table; exit
+    		throw new IllegalArgumentException("Vertex does not exist");
+    	}
+
+    	//otherwise need to find out all the vertices attached
+    	int v = (int) vertices.get(vertLabel);
+    	
+    	//after finding column for V check all other V for edges
+    	for(int i = 0; i <numOfV; ++i)
+    	{
+    		if(edges[i][v] == 1)
+    		{
+    			//get key based on value then add to ArrayList
+    			neighbours.add(getKeyFromValue(i));
+    		}
+    	}
+    	// Implement me!
+
+    	return neighbours; 
+
     } // end of neighbours()
     
     
-    public void removeVertex(T vertLabel) {
-      //  String vertLbl = vertLabel.toString();
-        //more elegant solution
-        //make use of size and volume (size current elements, volume ratio it goes by)
-        //grow by standard size each time and no ned to shrink 
-        //TO DELETE
-        //move row/column to delete to the end of the array, swapping with data currently in that position
-        //then just delete the the last row/column in array
-        //no need to resize everytime
-        if(vertices.get(vertLabel)== null)
-        {
-        	System.out.println("vertice doesn't exist");
-        	return;
-        }
-        
-        //get last element in Map for swapping
-        T tempVert  = getKeyFromValue(numOfV - 1);
-        //store value of V to remove to switch with element replacing it
-        int tempValue = vertices.get(vertLabel);
-    	/*
-        System.out.println(tempVert + " " +tempValue);
+    public void removeVertex(T vertLabel) throws IllegalArgumentException{
+    	//more elegant solution
+    	//make use of size and volume (size = current elements, 
+    	//								volume ratio it grows by)
+    	//grow by standard size each time and no need to shrink 
+    	//TO DELETE
+    	//move row/column to delete to the end of the array, 
+    	//	swapping with data currently in that position
+    	//then just delete the the last row/column in array
+    	//no need to resize everytime
     	
-    	System.out.println(edges.length+" "+numOfV);
-    	System.out.println(edges[numOfV -1][0]);
-    	System.out.println(edges[0][numOfV -1]);
-		*/
+    	//check that vertice exists
+    	if(vertices.get(vertLabel) == null)
+    	{
+    		throw new IllegalArgumentException("Vertex does not exist");
+    	}
+
+    	//get last V in Map for swapping
+    	T tempVert  = getKeyFromValue(numOfV - 1);
+    	//store value of V to remove to switch with element replacing it
+    	int tempValue = vertices.get(vertLabel);
     	
-        //if 
-        if(numOfV-1 != tempValue)
-        {
-            for(int i = 0;i<numOfV; ++i)
-            {
-                 edges[tempValue][i] = edges[(numOfV-1)][i];
-                 edges[i][tempValue] = edges[i][(numOfV-1)];
-                 //need to update map with correct values
-            }
-        }
-        
-        for(int i =0;i<numOfV;++i)
-        {
-        	edges[numOfV-1][i] = 0;
-        	edges[i][numOfV-1] = 0;
-        }
-        //System.out.println(vertices.toString() + tempVert + tempValue);
-        vertices.remove(vertLabel);
-        vertices.put(tempVert, tempValue);
-        
-        numOfV--;
-        //System.out.println(vertices.toString());
-        /*
-        int v = (int)vertices.get(vertLbl);
-        if(numOfV != v)
-        {
-            for(int i = 0;i<numOfV; ++i)
-            {
-                 edges[v][i] = edges[numOfV][i];
-                 edges[i][v] = edges[i][numOfV];
-                 //need to update map with correct values
-            }
-        }
-        
-        for(int i =0;i<numOfV;++i)
-        {
-        	edges[numOfV][i] = 0;
-        	edges[i][numOfV] = 0;
-        }
-        
-        T tempKey = getKeyFromValue(numOfV);
-        int tempInt  = vertices.get(vertLabel);
-        vertices.remove(tempKey);
-        vertices.remove(vertLabel);
-        
-        System.out.println(vertices.toString());
-        vertices.put(tempKey, tempInt);
-        System.out.println(vertices.toString());
+    	//if V to remove is not last V in map, need to swap with last V
+    	if(numOfV-1 != tempValue)
+    	{
+    		for(int i = 0;i<numOfV; ++i)
+    		{
+    			if(i != tempValue)
+    			{
+    				edges[tempValue][i] = edges[(numOfV-1)][i];
+    				edges[i][tempValue] = edges[i][(numOfV-1)];
+    			    //need to update map with correct values
+    			}
+    			else if(i == numOfV -1)
+    			{
+    				edges[tempValue][tempValue] = edges[i][i];
+    			}
+    		}
+    	}
+    	
+    	//ensure no incorrect values in array
+    	for(int i =0;i<numOfV;++i)
+    	{
+    		edges[numOfV-1][i] = 0;
+    		edges[i][numOfV-1] = 0;
+    	}
+    	
+    	//update map with new value for last element
+    	vertices.put(tempVert, tempValue);
 
-        numOfV--;
+    	//remove V from map
+    	vertices.remove(vertLabel);
+    	
+    	//decrement number of vertices
+    	numOfV--;
         
-        */
-        /*
-        if(v==null){
-            //node doesn't exsisst
-            return;
-        }
-
-
-        int[][] tempV = new int[numOfV -1][numOfV -1];
-        int newI = 0, newJ = 0;
-        for(int i =0;i <numOfV;i++){
-            if(i!=v){
-                for(int j = 0;j<numOfV;j++){
-                    if(i==v||j==v){
-                        
-                    }
-                    else if{
-                        tempV[newI][newJ] = edges[i][j];
-                        newJ++;
-                    }
-                    
-                }
-                newJ = 0;
-                newI++;
-            }
-        }
-        */
-        // Implement me!
     } // end of removeVertex()
 	
     
-    public void removeEdge(T srcLabel, T tarLabel) {
-        String srcLbl = srcLabel.toString();
-        String tarLbl = tarLabel.toString();
-
-        Integer x = (int)vertices.get(srcLbl);
-        Integer y = (int)vertices.get(tarLbl);
+    public void removeEdge(T srcLabel, T tarLabel) throws IllegalArgumentException {
         
-
-        if(x==null||y==null){
-            System.out.println("vertice not found");
-        	return;
+    	if(vertices.get(srcLabel) == null || vertices.get(tarLabel) == null)
+        {
+        	throw new IllegalArgumentException("One or both dont exist");
         }
-
+    	
+    	//get location of V in array
+    	Integer x = (int)vertices.get(srcLabel);
+        Integer y = (int)vertices.get(tarLabel);
+        
+        
+        
+        //change both directions of the graph
         edges[x][y] = 0;
         edges[y][x] = 0;
         
@@ -364,23 +231,28 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     
     public void printEdges(PrintWriter os) {
         
+    	//go through array to find edges
     	for(int i =0;i<numOfV;++i)
     	{
     		for(int j =0;j<numOfV;++j)
     		{
     			//System.out.println('i' + 'j' + numOfV);
+    			//if edge found get V turn to string and push to os
     			if(edges[i][j] == 1)
     			{
     				String edge1 = getKeyFromValue(i).toString();
     				String edge2 = getKeyFromValue(j).toString();
-    				os.print(edge1 +" ");
-    				os.println(edge2);
+    				
+    				os.println(edge1+" "+edge2);
     			}
     		}
     	}
     	// Implement me!
     } // end of printEdges()
     
+    
+    //my own method, used to find the Key (or V) based on the value
+    //slow as it will need to check all Keys to find correct value
     public T getKeyFromValue(int value)
     {
     	for(T o : vertices.keySet())
@@ -393,35 +265,43 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     	return null;
     }
     
-    public int shortestPathDistance(T vertLabel1, T vertLabel2) {
-    	// Implement me!
+    public int shortestPathDistance(T vertLabel1, T vertLabel2) throws IllegalArgumentException{
+    	// Check if both V are in Map
+        if(!vertices.containsKey(vertLabel1) || !vertices.containsKey(vertLabel2))
+        {
+        	throw new IllegalArgumentException("One or both dont exist");
+        }
         
+        //create array of visted nodes
     	boolean[] marked = new boolean[numOfV];
     	for(int i =0;i<numOfV; i++)
     	{
     		marked[i] = false;
     	}
     	
+    	//create queue of nodes to check
     	ArrayDeque<KeyEntry> list = new ArrayDeque<KeyEntry>();
     	
+    	//put first node in queue and start distance counter
     	int d =0;
     	list.add(new KeyEntry(vertLabel1, d));
     	
-    	
-    	
+    	//keep going until no more nodes to check
     	while(!list.isEmpty())
     	{
     		KeyEntry entryKey = list.remove();
     		
     		T currentT = entryKey.getVertex();
-    		
+    		//if dest the print out distance value
     		if(currentT.equals(vertLabel2) )
     		{
     			return entryKey.getDistance();
     		}
     		
+    		//otherwise mark as visted and check neighbours
     		marked[vertices.get(currentT)] = true;
     		
+    		//repeat process for finding nieghbours and add each to the queue
     		for(int i =0;i<numOfV; ++i)
     		{
     			if(edges[vertices.get(currentT)][i] == 1)
@@ -439,7 +319,7 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         return disconnectedDist;    	
     } // end of shortestPathDistance()
     
-    
+    //class to handle the data stored in the queue for BFS
     class KeyEntry
     { 
     	private T vertex; 

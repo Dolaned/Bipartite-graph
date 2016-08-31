2473b0de29accb27e46275d62eaa32f0c2635887
therobotdoctor <michaelbannister01@hotmail.com>
2016-09-01 01:33:25 +1000

    Update Main.java
    density data generation file

--- testing/Main.java
+++ testing/Main.java
@@ -1,42 +1,44 @@
 
-
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
-public class Main {
+public class DataGen {
 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-        PrintWriter writer = new PrintWriter("1000000v.txt", "UTF-8");
+        //different densitis of facebook graph 2% 5% and 9%
+    	//of values 6,000 
+    	PrintWriter writer = new PrintWriter("4000v.02%_2.in", "UTF-8");
         Random rn = new Random();
 
-	    for(int i = 5000; i < 500000; i ++){
+	    for(int i = 4039; i < 4039; i ++){
             writer.println("AV "+i);
         }
-        for(int i = 0; i < 500000; i ++){
-            writer.println("AE " +i+ " "+ rn.nextInt(499999));
+	    
+        for(long i = 0; i < 71800; i ++){
+            writer.println("AE " +(i%4038)+ " "+ rn.nextInt(4037));
         }
         writer.close();
 
-        PrintWriter writer2 = new PrintWriter("10000v.txt", "UTF-8");
+        PrintWriter writer2 = new PrintWriter("4000v.1%_2.in", "UTF-8");
 
-        for(int i = 5000; i < 15000; i ++){
+        for(int i = 4039; i < 4039; i ++){
             writer2.println("AV "+i);
         }
-        for(int i = 5000; i < 15000; i ++){
-            writer2.println("AE " +i+ " "+ rn.nextInt(14999));
+        for(int i = 0; i < 711800; i ++){
+            writer2.println("AE " + (i%4038) + " "+ rn.nextInt(4037));
         }
         writer2.close();
 
-        PrintWriter writer3 = new PrintWriter("100000v.txt", "UTF-8");
+        PrintWriter writer3 = new PrintWriter("4000v.5%_2.in", "UTF-8");
 
 
-        for(int i = 0; i < 100000; i ++){
+        for(int i = 4039; i < 4039; i ++){
             writer3.println("AV "+i);
         }
-        for(int i = 0; i < 100000; i ++){
-            writer3.println("AE " +i+ " "+ rn.nextInt(99999));
+        for(int i = 0; i < 3911800; i ++){
+            writer3.println("AE " +(i%4038)+ " "+ rn.nextInt(4037));
         }
         writer3.close();
     }
@@ -44,3 +46,4 @@ public class Main {
 
 
 }
+

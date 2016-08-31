b6a17c42f65e6a4fd8b74b596a0019c3c6ad3d81
Gl3nda85 <s3249319@student.rmit.edu.au>
2016-08-31 14:00:26 +1000

    Adjlist complete and passes jeffreys tests

--- javaSrc/AdjList.java
+++ javaSrc/AdjList.java
@@ -1,6 +1,7 @@
 import java.io.PrintWriter;
 import java.security.KeyPair;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Adjacency list implementation for the FriendshipGraph interface.
@@ -26,10 +27,10 @@ public class AdjList<T extends Object> implements FriendshipGraph<T> {
 
 
     public void addVertex(T vertLabel) {
-
         if (keyValues.get(vertLabel) != null) {
-            System.out.println("Vertex Exists");
+            //System.out.println("Vertex Exists");
         } else {
+            long startTime = System.nanoTime();
             if (adjListSize == 0) {
                 aList[0] = new MyLinkedList();
                 keyValues.put(vertLabel, adjListSize);
@@ -51,14 +52,18 @@ public class AdjList<T extends Object> implements FriendshipGraph<T> {
                 aList[adjListSize] = new MyLinkedList();
             }
             adjListSize++;
+            long endTime = System.nanoTime();
+            long durationInMs = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+            System.out.println("Add vertex takes: " + durationInMs + "Ms");
         }
+
     } // end of addVertex()
 
 
     public void addEdge(T srcLabel, T tarLabel) {
-
+        long startTime = System.nanoTime();
         if (keyValues.get(srcLabel) == null || keyValues.get(tarLabel) == null) {
-            System.out.println("Index Not Found");
+            throw new IllegalArgumentException("Index Not Found");
         } else {
 
             int vertSrcEdge = keyValues.get(srcLabel);
@@ -68,16 +73,20 @@ public class AdjList<T extends Object> implements FriendshipGraph<T> {
                 aList[vertSrcEdge].add(vertTarEdge);
                 aList[vertTarEdge].add(vertSrcEdge);
             } else {
-                System.out.println("Edge already Exists");
+                throw new IllegalArgumentException("Edge already Exists");
             }
         }
+        long endTime = System.nanoTime();
+        long durationInMs = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+        System.out.println("Add edge takes: " + durationInMs + "Ms");
     } // end of addEdge()
 
     public ArrayList<T> neighbours(T vertLabel) {
+        //long startTime = System.nanoTime();
         ArrayList<T> neighbours = new ArrayList<T>();
 
         if (keyValues.get(vertLabel) == null) {
-            System.out.println("vertex not found");
+            throw new IllegalArgumentException("vertex not found");
         } else {
             int vertexForNeighbours = keyValues.get(vertLabel);
             //System.out.println("neighbour Label: "+vertLabel.toString() +" vertex value:  " + vertexForNeighbours );
@@ -90,14 +99,18 @@ public class AdjList<T extends Object> implements FriendshipGraph<T> {
                 }
             }
         }
+        /*
+        long endTime = System.nanoTime();
+        long durationInMs = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+        System.out.println("get neighbours takes: " + durationInMs + "Ms");*/
         return neighbours;
     } // end of neighbours()
 
 
     public void removeVertex(T vertLabel) {
-
+        long startTime = System.nanoTime();
         if (keyValues.get(vertLabel) == null) {
-            System.out.println("Vertex Not Found");
+            throw new IllegalArgumentException("Vertex Not Found");
         } else {
             int arrayVal = keyValues.get(vertLabel);
             ArrayList<T> neighbours = neighbours(vertLabel);
@@ -143,15 +156,19 @@ public class AdjList<T extends Object> implements FriendshipGraph<T> {
             adjListSize--;
             keyValues.remove(vertLabel);
         }
+        long endTime = System.nanoTime();
+        long durationInMs = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+        System.out.println("remove vertex takes: " + durationInMs + "Ms");
 
 
     } // end of removeVertex()
 
 
     public void removeEdge(T srcLabel, T tarLabel) {
+        long startTime = System.nanoTime();
 
         if (keyValues.get(srcLabel) == null || keyValues.get(tarLabel) == null) {
-            System.out.println("1 or more vertexes not found");
+            throw new IllegalArgumentException("1 or more vertexes not found");
         } else {
             int vertSrcEdge = keyValues.get(srcLabel);
             int vertTarEdge = keyValues.get(tarLabel);
@@ -159,6 +176,9 @@ public class AdjList<T extends Object> implements FriendshipGraph<T> {
             aList[vertSrcEdge].remove(vertTarEdge);
             aList[vertTarEdge].remove(vertSrcEdge);
         }
+        long endTime = System.nanoTime();
+        long durationInMs = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+        System.out.println("remove edge takes: " + durationInMs + "Ms");
     } // end of removeEdges()
 
 
@@ -188,7 +208,7 @@ public class AdjList<T extends Object> implements FriendshipGraph<T> {
 
 
     public int shortestPathDistance(T vertLabel1, T vertLabel2) {
-
+        long startTime = System.nanoTime();
         ArrayDeque<KeyEntry> q = new ArrayDeque<>();
         HashMap<T, Boolean> visited = new HashMap<>();
 
@@ -201,8 +221,11 @@ public class AdjList<T extends Object> implements FriendshipGraph<T> {
 
             //assign current to the entries vertex
             T current = entry.getVertex();
-
+            //check if current node equals target node
             if (current.equals(vertLabel2)) {
+                long endTime = System.nanoTime();
+                long durationInMs = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+                System.out.println("shortest path takes: " + durationInMs + "Ms");
                 return entry.getDistance();
             }else{
                 for (T node : neighbours(current)) {
@@ -213,11 +236,14 @@ public class AdjList<T extends Object> implements FriendshipGraph<T> {
                 }
             }
         }
+        long endTime = System.nanoTime();
+        long durationInMs = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+        System.out.println("shortest path takes: " + durationInMs + "Ms");
         // if we reach this point, source and target are disconnected
         return disconnectedDist;
     } // end of shortestPathDistance()
 
-class KeyEntry{
+private class KeyEntry{
     private T vertex;
     private int distance;
 
